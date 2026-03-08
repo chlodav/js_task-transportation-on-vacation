98e8fe57f235ad72c7baa084c734cb3ac001ee53
CD <153681219+chlodav@users.noreply.github.com>
2026-03-08 08:06:56 -0700

    added function calculateCosts

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,16 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const dailyRate = 40;
+  const baseCost = days * dailyRate;
+
+  if (days >= 7) {
+    return baseCost - 50;
+  } else if (days >= 3) {
+    return baseCost - 20;
+  } else {
+    return baseCost;
+  }
 }
 
 module.exports = calculateRentalCost;
